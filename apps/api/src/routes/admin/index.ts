49b51c260ff60611d1e25b87a634b5bcf2cf8521
thehazell <taco.xr@proton.me>
2026-09-05 12:44:46 -0500

    feat: list users endpoint

--- apps/api/src/routes/admin/index.ts
+++ apps/api/src/routes/admin/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import bootstrap from "./bootstrap";
 import lifecycle from "./lifecycle";
+import users from "./users";
 
 const admin = new Hono<{ Bindings: Env }>();
 
 admin.route("/bootstrap", bootstrap);
 admin.route("/", lifecycle);
+admin.route("/users", users)
 
 export default admin;
