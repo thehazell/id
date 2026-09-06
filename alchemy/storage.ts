71df8b685aed33ca120a401b5cab84c72a38b6df
thehazell <taco.xr@proton.me>
2026-09-06 18:21:12 -0500

    fix: you can pass forceDestroy to the R2 stack to empty the bucket

--- alchemy/storage.ts
+++ alchemy/storage.ts
@@ -1,3 +1,5 @@
 import * as Cloudflare from "alchemy/Cloudflare";
 
-export const ProfileBucket = Cloudflare.R2.Bucket("ProfileBucket");
+export const ProfileBucket = Cloudflare.R2.Bucket("ProfileBucket", {
+	//	forceDestroy: true,
+});
