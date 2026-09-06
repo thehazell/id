d4a4c36680ba63af68be1208132b497c7fdffa04
thehazell <taco.xr@proton.me>
2026-09-06 08:44:52 -0500

    feat: reorganize well-known routes

--- apps/api/src/routes/well-known/openid-configuration/index.ts
+++ apps/api/src/routes/well-known/openid-configuration/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 
-import jwksRoute from "./jwks";
+import getRoute from "./get";
 
 const route = new Hono<{ Bindings: Env }>();
 
-route.route("/jwks.json", jwksRoute);
+route.route("/", getRoute);
 
 export default route;
